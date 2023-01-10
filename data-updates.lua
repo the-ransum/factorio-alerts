ed18185b89c5aeefd66423985162dd5d7b8347ee
the-ransum <kevin.haas96@gmail.com>
2023-01-09 22:28:04 -0500

    fixed file ids

--- data-updates.lua
+++ data-updates.lua
@@ -3,7 +3,7 @@ local speaker = data.raw["programmable-speaker"]["programmable-speaker"]
 speaker.instruments[#speaker.instruments+1] = {
 	name = "simple_alert_sounds",
 	notes = {
-		{name = "Nuclear Launch Detected", sound = {filename = "__Simple_Alert_Sounds__/sound/nuclear_launch_detected.ogg"}},
-		{name = "Red Alert Danger", sound = {filename = "__Simple_Alert_Sounds__/sound/red_alert_danger.ogg"}},
+		{name = "nuclear_launch_detected", sound = {filename = "__Simple_Alert_Sounds__/sound/nuclear_launch_detected.ogg"}},
+		{name = "red_alert_danger", sound = {filename = "__Simple_Alert_Sounds__/sound/red_alert_danger.ogg"}},
     }
 }
