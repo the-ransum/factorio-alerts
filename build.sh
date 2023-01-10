26082f4301b4e168fafad3199c887b4e751c311a
the-ransum <kevin.haas96@gmail.com>
2023-01-09 23:10:19 -0500

    size disrepency fix

--- build.sh
+++ build.sh
@@ -13,7 +13,8 @@ build_zip="${versions_dir}/${build_name}_${build_release}.zip"
 
 echo "Build Name: ${build_name}"
 echo "Build Release: ${build_release}"
+echo "Versions: ${versions_dir}"
 echo "Zipping Build to ${build_zip}"
 
-zip -r "$build_zip" "$cwd" -x "$versions_dir"
+zip -r "$build_zip" "$cwd" -x "${cwd}/versions" "${cwd}/versions/*" "${cwd}/.git" "${cwd}/.git/*"
 
