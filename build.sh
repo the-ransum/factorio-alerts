1c182546378a9f378b98649850dbbd5ab8bffd7a
the-ransum <kevin.haas96@gmail.com>
2023-01-09 22:01:53 -0500

    Added build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cwd=$(dirname "$0")
+versions_dir="${cwd}/versions"
+mod_info=$(cat "${cwd}/info.json")
+build_release=$(echo "${mod_info}" | jq -r '.version')
+build_name=$(echo "${mod_info}" | jq -r '.name')
+
+build_zip="${versions_dir}/${build_name}.${build_release}.zip"
+
+echo "Build Name: ${build_name}"
+echo "Build Release: ${build_release}"
+echo "Zipping Build to ${build_zip}"
+
+
+zip -r "$build_zip" "$cwd" -x "$versions_dir"
+
+# zip -r myarchive.zip dir1 -x dir1/ignoreDir1/**\* dir1/ignoreDir2/**\*
