011fd4b50df7ec2392b7841f93c32b9dd760f98d
the-ransum <kevin.haas96@gmail.com>
2023-01-09 22:58:38 -0500

    fixed issue with mod compression

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cwd=$(dirname "$0")
+script_name="$0"
+cwd="$(basename "$(dirname "$(realpath -s ${script_name})")")"
+cd ..
+
 versions_dir="${cwd}/versions"
 mod_info=$(cat "${cwd}/info.json")
 build_release=$(echo "${mod_info}" | jq -r '.version')
@@ -11,7 +14,6 @@ build_zip="${versions_dir}/${build_name}_${build_release}.zip"
 echo "Build Name: ${build_name}"
 echo "Build Release: ${build_release}"
 echo "Zipping Build to ${build_zip}"
-
 
 zip -r "$build_zip" "$cwd" -x "$versions_dir"
 
