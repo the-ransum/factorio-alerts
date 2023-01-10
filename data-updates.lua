8a552c5661862485a21bc4e749e603921de7bc98
the-ransum <kevin.haas96@gmail.com>
2023-01-09 21:26:00 -0500

    fixed typo

--- data-updates.lua
+++ data-updates.lua
@@ -1,6 +1,6 @@
 local speaker = data.raw["programmable-speaker"]["programmable-speaker"]
 speaker.instruments[#speaker.instruments+1] = {
-	name = "c&c-low-power-alerts",
+	name = "simple_alert_sounds",
 	notes = {
 		{name = "Nuclear Launch Detected", sound = {filename = "__Simple_Alert_Sounds__/sound/nuclear_launch_detected.ogg"}},
 		{name = "Red Alert Danger", sound = {filename = "__Simple_Alert_Sounds__/sound/red_alert_danger.ogg"}},
